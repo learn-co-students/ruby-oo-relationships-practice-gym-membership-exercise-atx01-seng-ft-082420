59e7453eb89b1c89fc6d3a0102fa5bbebcfc63fd
github username <github email address>
2020-09-01 10:20:49 -0500

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,14 @@
 class Membership
-  attr_reader :cost
-
-  def initialize(cost)
+  attr_reader :cost, :lifter, :gym
+  @@all = []
+  def initialize(cost, lifter, gym)
     @cost = cost
+    @lifter = lifter
+    @gym = gym
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,18 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+jake = Lifter.new("Jake", 920)
+frank = Lifter.new("Frank", 1000)
+steven = Lifter.new("steven", 750)
+
+g1 = Gym.new("Golds")
+g2 = Gym.new("24hr fitness")
+g3 = Gym.new("Ufc gym")
+
+m1 = Membership.new(29.99, jake, g3)
+m2 = Membership.new(29.99, jake, g1)
+m3 = Membership.new(39.99, frank, g3)
+m4 = Membership.new(29.99, steven, g2)
 
 binding.pry
 
