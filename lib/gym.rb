8e16507811681c287ebce20231304cffe10db5d8
whychooseaname <68562400+whychooseaname@users.noreply.github.com>
2020-08-31 23:24:35 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,49 @@
 class Gym
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.gym_names
+    arr=[]
+    self.all.select {|gym| arr << gym.name}
+    return arr
+  end
+
+  def memberships
+    arr=[]
+    Membership.all.select do |member| 
+      if member.gym == self
+        arr << member
+      end
+    end
+    return arr
+  end
+
+  def members 
+    arr = []
+    Membership.all.select do |member|
+      if member.gym == self
+        arr << member.lifter.name
+      end
+    end
+    return arr
+  end
+
+  def total_lift
+    sum = 0
+    Membership.all.each do |member|
+      #binding.pry
+      if member.gym == self
+        sum += member.lifter.lift_total
+      end
+    end
+    sum
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,28 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+twentyfour = Gym.new("24")
+anytime =Gym.new("Anytime")
+crunch = Gym.new("Crunch")
+bally = Gym.new("Bally Total Fitness")
+la = Gym.new("LA Fitness")
+golds = Gym.new("Golds Gym")
+
+greg = Lifter.new("Greg", 100)
+sally = Lifter.new("Sally", 150)
+jake = Lifter.new("Jake", 200)
+bianca = Lifter.new("Bianca",  175)
+mountain = Lifter.new("The Mountain", 800)
+
+Membership.new(15, greg, crunch)
+Membership.new(20, greg, anytime)
+Membership.new(20, sally, anytime)
+Membership.new(45, jake, twentyfour)
+Membership.new(35, bianca, bally)
+Membership.new(50, bianca, la)
+Membership.new(400, mountain, golds)
+Membership.new(100, mountain, twentyfour)
+
 
 binding.pry
-
 puts "Gains!"
