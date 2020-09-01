67170c64ce8439f0c96d9aa96de582359ce92bd6
lagofelipe <lagofelipe@gmail.com>
2020-09-01 16:55:56 -0500

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,9 +1,9 @@
 class Lifter
   attr_reader :name, :lift_total
+@@all = []
 
-  
-  @@all = []
-  def initialize(name, lift_total)
+
+def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
 
@@ -14,6 +14,29 @@ def self.all
   @@all
   end
 
+  def gyms
+    
+    self.memberships.collect do | membership |
+      membership.gym
+    end
+end
+             
+def self.average_gains
+
+  sum = self.all.sum{|lifter| lifter.lift_total}
+lift_total = self.all.count
+sum / total_count
+end
+
+def total_cost
+
+  cost = self.memberships.sum do |membership|
+  membership.cost
+end
+
+new_member(gym, cost)
+Membership.new(gym, self, cost)
+end
 
 
 end
