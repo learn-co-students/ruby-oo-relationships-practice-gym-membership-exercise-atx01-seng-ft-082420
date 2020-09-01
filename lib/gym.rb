139f689d01a03fe773d5510dcce7c3d2fe19247d
jlonetree <jlonetree@gmail.com>
2020-09-01 12:17:31 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,40 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |m|
+      m.gym == self
+    end
+  end
+
+  def members
+    self.memberships.collect do |mem|
+      mem.lifter
+    end
+  end
+
+  def member_names
+    self.members.collect do |mn|
+      mn.name
+    end
+  end
+
+  def lift_total
+    self.memberships.sum do |mem|
+      mem.lifter.lift_total
+    end
+  end
+
 end
